e9c89a0aae008301e4b2f2e430de25e396142a14
Putu Eka Suryadana <esuryadana@gmail.com>
2022-02-16 08:40:51 +0800

    add link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './Banner.css';
 import { Button } from '@material-ui/core';
 import Search from './Search';
+import { useHistory } from 'react-router-dom';
 
 function Banner() {
 
+    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false);
 
     return (
@@ -19,7 +21,7 @@ function Banner() {
             <div className='banner__info'>
                 <h1>This my Portofolio App Air BnB using React JS</h1>
                 <h5>Plan a different kind of gate to uncover the hidden gem near you</h5>
-                <Button
+                <Button onClick={() => history.push('/search')}
                 variant='outlined'>
                     Explore Nearby
                 </Button>
